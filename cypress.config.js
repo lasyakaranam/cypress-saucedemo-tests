ec5db3cfa2935baaefa39f9e5257fc28ad81bf32
lasyakaranam <2200010029@kluniversity.in>
2025-09-14 11:14:29 +0530

    Fix Github Actions workflow for cypress tests

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000', // Change to your app URL
-    specPattern: 'cypress/e2e/**/*.cy.js', // Test files location
+    baseUrl: 'https://www.saucedemo.com', // no need to start a local app
+    specPattern: 'cypress/e2e/**/*.cy.js',
     supportFile: 'cypress/support/e2e.js',
-    setupNodeEvents(on, config) {
-      // Node event listeners can go here
-    }
   }
 })
